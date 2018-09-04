cad8fd99a0397c5c8ca08615caef7c067fe4ba83
Jens Filipsson <jensfilipsson@gmail.com>
2018-09-04 15:44:14 +0200

    Fixing ESLint Errors

--- frontend/src/components/StartScreen.js
+++ frontend/src/components/StartScreen.js
@@ -21,19 +21,46 @@ class StartScreen extends React.Component {
         name: 'Harmonisk',
         color: 'orange',
       },
+      {
+        name: 'Sprallig',
+        color: 'yellow',
+      },
+      {
+        name: 'Kärleksfylld',
+        color: 'pink',
+      },
+      {
+        name: 'Orolig',
+        color: 'turquoise',
+      },
+      {
+        name: 'Stressad',
+        color: 'purple',
+      },
     ],
     selected: [],
-    user: {name:'Nathalie',}
+    user: { name: 'Nathalie' },
+    randomHelloPhrase: 'hur mår du idag?',
   };
 
+  handleChecked = () => {};
+
   render() {
-    const { emotions, user } = this.state;
-    const emotionsOutput = emotions.map((item, i) => (
-      <EmotionButton item={item} key={i} />
+    const { emotions, user, randomHelloPhrase } = this.state; // destructuring state
+    const emotionsOutput = emotions.map((item) => (
+      <EmotionButton
+        item={item}
+        key={item.name}
+        returnFunction={this.handleChecked}
+      />
     ));
     return (
       <div className="start-screen">
-        <h2>Hej {user.name}, <span>hur mår du idag?</span></h2>
+        <h2>
+          Hej
+          {` ${user.name},`}
+          <span>{randomHelloPhrase}</span>
+        </h2>
         <div className="startscreen-emotion-list">{emotionsOutput}</div>
       </div>
     );
