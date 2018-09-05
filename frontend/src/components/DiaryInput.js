17f5b685dcbc8d38024c578c98c5aef3209be1e3
agnekil <nathalie@agnekil.com>
2018-09-05 09:50:02 +0200

    fixat kaoset

--- frontend/src/components/DiaryInput.js
+++ frontend/src/components/DiaryInput.js
@@ -1,46 +1,51 @@
 import React from 'react';
-import {NextButton} from "./NextButton";
+import { NextButton } from './NextButton';
 
-//Input where user can enter a post for the day if they want to
+// Input where user can enter a post for the day if they want to
 class DiaryInput extends React.Component {
   state = {
     diary: '',
   };
 
-  onChange = () => {
+  onChange = (event) => {
     this.setState({ diary: event.target.value });
   };
 
-  saveDiary = () => {
+  saveDiary = (event) => {
+    const { diary } = this.state;
     event.preventDefault();
     fetch('/savediary', {
       method: 'POST',
       // credentials: "same-origin",
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(this.state.diary)
+      body: JSON.stringify({ diary }),
     })
       .then((response) => response.json())
-      .then((clear => {
+      .then(() => {
         this.setState({ diary: '' });
       })
       .catch((error) => {
         console.log(error);
-      })
+      });
   };
 
   render() {
+    const { diary } = this.state;
     return (
       <div className="diary-input-container">
         <form>
-          <label htmlFor="diary">Enter a journal post</label>
-          <input
-            className="diary-input"
-            name="diary"
-            type="text"
-            placeholder="Vill du tillägga något?"
-            onChange={this.onChange}
-            value={this.state.diary}
-          />
+          <label htmlFor="diary-input">
+            Enter a journal post
+            <input
+              className="diary-input"
+              name="diary-input"
+              id="diary-input"
+              type="text"
+              placeholder="Vill du tillägga något?"
+              onChange={this.onChange}
+              value={diary}
+            />
+          </label>
           <NextButton onClick={this.saveDiary} />
         </form>
       </div>
