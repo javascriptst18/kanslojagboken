46bc0cf156ed9d0874cfbc3407c27dd249dbc62a
agnekil <nathalie@agnekil.com>
2018-09-06 11:16:56 +0200

    Installerat dependencies och gjort komponent för Word Cloud

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import 'normalize.css';
 import './App.css';
 import StartScreen from './components/StartScreen';
-import DiaryInput from './components/DiaryInput';
 
 class App extends React.Component {
   render() {
