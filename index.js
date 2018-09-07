cd6d47cf529f17e02a079dc12e2d305d54d2dbba
Carl-Axel Hallgren <carl.axel.hallgren@gmail.com>
2018-09-07 13:33:10 +0200

    backend fetch emo frq by date

--- index.js
+++ index.js
@@ -25,11 +25,12 @@ app.use(cors());
 /* 
         Documentation
 
-        GET /userdata with req.query.id as userid, gets all data for that user
-        GET /userdatabydate TODO
-        POST /newuser with req.body.data as first emotion array
-        PATCH /updateuserdata (for updating data) try this first on error try the next, req.body.data as emotion array req.body.id as userid
-        POST /updateuserdata (for new data for that day) req.body.data as emotion array req.body.id as userid
+        GET /userdata with        req.query.id as userid, gets all data for that user
+        GET /userdatabydate       emotion freq within timeperiod, req.query.id and (ex: 20180702 as req.query.datestart and 20180830 as req.query.dateend)
+        POST /newuser    with     req.body.data as first emotion array
+        PATCH /updateuserdata     (for updating data) try this first on error try the next, req.body.data as emotion array req.body.id as userid
+        POST /updateuserdata      (for new data for that day) req.body.data as emotion array req.body.id as userid
+        PATCH /updateusercolor    TODO
 
 */
 
@@ -47,14 +48,49 @@ app.get('/userdata', async (req, res, err) => {
 });
 
 
-// GET userdata req.query.id by date
+// GET emotion freq within timeperiod, req.query.id and (ex: 20180702 as req.query.datestart and 20180830 as req.query.dateend)
 app.get('/userdatabydate', (req, res, err) => {
+  let startYear = req.query.datestart.substr(0,4);
+  let startMonth = req.query.datestart.substr(4,2);
+  let startDay = req.query.datestart.substr(6,2);
+  let start = new Date(startYear,startMonth,startDay)
+  let endYear = req.query.dateend.substr(0,4);
+  let endMonth = req.query.dateend.substr(4,2);
+  let endDay = req.query.dateend.substr(6,2);
+  let end = new Date(endYear,endMonth,endDay)
+  
+  
   MongoClient.connect(uri,{ useNewUrlParser: true },async function(err, client) {
     assert.equal(null, err);
     const collection = client.db("users").collection("userdata");
-    collection.find(ObjectId(req.query.id)).toArray(function(err, result){
-      res.send(JSON.stringify(result));
-    }) 
+    let result = await collection.findOne(ObjectId(req.query.id))
+    result = result.emotionData;
+    console.log(result);
+    result = result.filter((date)=>{
+      if(date.date >= start && date.date <= end){
+        return date;
+      }
+      
+    })
+    console.log(result)
+    result = result.reduce((accumulatedArray,emotion)=>{
+      accumulatedArray = accumulatedArray.concat(emotion["emotions"]);
+      return accumulatedArray;
+    },[])
+
+    result = result.reduce((accumulatedObject,emotion)=>{
+      if(accumulatedObject[emotion]){
+        let value = accumulatedObject[emotion];
+        value = value+1;
+        accumulatedObject[emotion]=value;
+      }else{
+        accumulatedObject[emotion]=1
+      }
+      return accumulatedObject;
+      
+    },{})
+    res.send(result);
+
   client.close();
   });
   
@@ -62,17 +98,16 @@ app.get('/userdatabydate', (req, res, err) => {
 
 app.post('/newuser', async (req, res, err) => {
   
-  let incoming = await JSON.parse(JSON.stringify(req.body.data));
-  console.log(incoming);
+  let incoming = req.body.data;
   let date = await new Date();
-  date=date.toString().split(" ").join("").substr(0,12);
-  let newUser = {emotionData:[{date:date,emotions:[incoming]}],colors:{0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[]}}
+  date.setHours(0,0,0,0);
+  let newUser = {emotionData:[{date:date,emotions:incoming}],colors:{0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[]}}
 
   MongoClient.connect(uri,{ useNewUrlParser: true },async function(err, client) {
     assert.equal(null, err);
     const collection = client.db("users").collection("userdata") 
-    collection.insert(newUser, function(err, result){
-      res.send(JSON.stringify(result));
+    collection.insertOne(newUser, function(err, result){
+      res.send(result);
     })
   
   client.close();
@@ -86,51 +121,70 @@ app.post('/newuser', async (req, res, err) => {
 
 app.patch('/updateuserdata', async (req, res, err) => {
   
-  console.log(req.body.data);
-  let date = await new Date();
-
-date=date.toString()
-date=date.split(" ").join("").substr(0,12);
-console.log(date);
-
+  let date = await new Date().setHours(0,0,0,0);
 
   MongoClient.connect(uri,{ useNewUrlParser: true },async function(err, client) {
     assert.equal(null, err);
     const collection = client.db("users").collection("userdata");
     collection.findOneAndUpdate({$and:[{"_id": ObjectId(req.body.id)}, {"emotionData.date":date}]},{$set:{"emotionData.$.emotions":req.body.data }},{returnOriginal: false},function(err,result){
-      console.log(result);
       if(result.lastErrorObject.updatedExisting){
-        res.send(JSON.stringify(result));
+        res.send(result);
       }else{
-        res.send(JSON.stringify("error"));
+        res.send("error");
       }
-    })
-  
-    
+    }) 
       client.close();
     })
   
   });
 
   app.post('/updateuserdata', async (req, res, err) => {
-    let date = await new Date();
-  
-  date=date.toString()
-  date=date.split(" ").join("").substr(0,12);
-  objEmotionData = {date:date,emotions:req.body.data}
+    let incoming = req.body.data;
+    let date = await new Date().setHours(0,0,0,0)
+    let objEmotionData = {date:date,emotions:incoming}
   
     MongoClient.connect(uri,{ useNewUrlParser: true },async function(err, client) {
       assert.equal(null, err);
       const collection = client.db("users").collection("userdata");
       collection.findOneAndUpdate({"_id": ObjectId(req.body.id)},{$push:{"emotionData":objEmotionData }},{returnOriginal: false}, function(err, result){
          
-          res.send(JSON.stringify(result));
+          res.send(result);
         })
         client.close();
       })
     
     
     });
+
+
+// -------------------------------------------------------- TODO ---------------------------------------------------------------------------
+
+    app.patch('/updateusercolor', async (req, res, err) => {
+  
+      console.log(req.body.data);
+      let date = await new Date()
+    
+      date.setHours(0,0,0,0);
+    console.log(date);
+    
+    
+      MongoClient.connect(uri,{ useNewUrlParser: true },async function(err, client) {
+        assert.equal(null, err);
+        const collection = client.db("users").collection("userdata");
+        collection.findOneAndUpdate({$and:[{"_id": ObjectId(req.body.id)}, {"emotionData.date":date}]},{$set:{"emotionData.$.emotions":req.body.data }},{returnOriginal: false},function(err,result){
+          if(result.lastErrorObject.updatedExisting){
+            res.send(result);
+          }else{
+            res.send("error");
+          }
+        })
+      
+        
+          client.close();
+        })
+      
+      });
+    
 
 
 
