34fcf6fd76dc99e1c36519832954615066040096
agnekil <nathalie@agnekil.com>
2018-09-10 16:27:46 +0200

    fixat arrayen till objekt så nu funkar både ord och storlek. bara färgen kvar!

--- frontend/src/components/WordCloud.js
+++ frontend/src/components/WordCloud.js
@@ -18,27 +18,8 @@ class WordCloud2 extends React.Component {
     const response = await result.json();
     this.setState({ words: response });
     console.log('words: ', response);
-    this.generatingFontSize();
     this.fetchColor();
-    this.fetchEmotions();
   }
-
-  fetchEmotions = () => {
-    const { words } = this.state;
-    const emotions = Object.keys(words);
-    console.log('fetchEmotions: ', emotions);
-    this.setState({ emotions });
-  };
-
-  // I den här metoden ska font size genereras utefter antal gånger varje ord använts. Tanken är att den ska loopa igenom varje ord, hitta dens siffra och multiplicera med 5 så får den denna font size.
-  // Sätt en max-gräns också.
-  generatingFontSize = () => {
-    const { words, fontsize } = this.state;
-    const values = Object.values(words);
-    console.log('generatingFontSize: ', values);
-    const results = values.map((word) => Math.log2(word) * 500);
-    this.setState({ fontsize: results });
-  };
 
   // Den här metoden ska sätta rätt färg på respektive ord. Det betyder att den måste loopa igenom color-databasen och match mot orden som ligger i föregående metod?
   fetchColor = () => {
@@ -58,11 +39,11 @@ class WordCloud2 extends React.Component {
       const array = Object.entries(words)[i];
       finalArray.push(array);
     }
-    // console.log(finalArray);
+    console.log(finalArray);
     // försöker göra om så att "text" blir key, alltså känslan, och att "value" blir värdet, alltså siffran.
     const newData = finalArray.map((item) => ({
-      text: item,
-      value: Math.random() * 2000,
+      text: item[0],
+      value: item[1],
     }));
     console.log(newData);
     return (
@@ -76,37 +57,13 @@ class WordCloud2 extends React.Component {
   }
 }
 
-// Dummy data for word cloud
-const data = [
-  { text: 'Hey', value: 1000 },
-  { text: 'lol', value: 200 },
-  { text: 'first impression', value: 800 },
-  { text: 'very cool', value: 1000000 },
-  { text: 'duck', value: 10 },
-  { text: 'Hey', value: 1000 },
-  { text: 'lol', value: 200 },
-  { text: 'first impression', value: 800 },
-  { text: 'very cool', value: 1000000 },
-  { text: 'duck', value: 10 },
-  { text: 'Hey', value: 1000 },
-  { text: 'lol', value: 200 },
-  { text: 'first impression', value: 800 },
-  { text: 'very cool', value: 1000000 },
-  { text: 'duck', value: 10 },
-  { text: 'Hey', value: 1000 },
-  { text: 'lol', value: 200 },
-  { text: 'first impression', value: 800 },
-  { text: 'very cool', value: 1000000 },
-  { text: 'duck', value: 10 },
-];
-
 // What's happening when you click a word
 function onWordClick() {
   console.log('Helluu');
 }
 
 // Randomizes the font size of the words, will not be necessary when the words will be adjusted after how many times they've been used.
-const fontSizeMapper = (word) => Math.log2(word.value) * 10;
+const fontSizeMapper = (word) => Math.log2(word.value) * 20;
 const onClick = (word) => onWordClick(word);
 // Rotating words horizontally and vertically (in same direction)
 const rotate = () => (Math.floor(Math.random() * 2) % 2 === 1 ? 90 : 0);
