dfe3546d06a78fbff4af352c69d5b9fbd988f512
Jens Filipsson <jensfilipsson@gmail.com>
2018-09-12 08:55:27 +0200

    Added loading animation module

--- frontend/src/components/LoadingAnimation.js
+++ frontend/src/components/LoadingAnimation.js
@@ -1,2 +1,19 @@
 import React from 'react';
+import './css/LoadingAnimation.css';
 
+function LoadingAnimation() {
+  return (
+    <div className="spinner">
+      <i />
+      <i />
+      <i />
+      <i />
+      <i />
+      <i />
+      <i />
+      <i />
+    </div>
+  );
+}
+
+export default LoadingAnimation;
--- frontend/src/components/SplashScreen.js
+++ frontend/src/components/SplashScreen.js
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react';
 import './css/SplashScreen.css';
 import logo from '../images/kanslojagboken-logo.png';
+import LoadingAnimation from './LoadingAnimation';
 // Function for outputting the splash screen on page load
 function SplashScreen() {
   return (
     <div className="splash-screen">
       <img src={logo} alt="Känslojagboken logo" />
+      <LoadingAnimation />
     </div>
   );
 }
