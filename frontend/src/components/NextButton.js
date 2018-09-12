82cee96cda93b0040aa4358efed68907ffc0f1e6
agnekil <nathalie@agnekil.com>
2018-09-12 10:27:33 +0200

    Fixat CSS på DiaryInput

--- frontend/src/components/NextButton.js
+++ frontend/src/components/NextButton.js
@@ -6,7 +6,7 @@ function NextButton(props) {
   return (
     <div>
       <button className="next-button" type="submit" onClick={onClick}>
-        Klicka här
+        Gå vidare
         <i className="fa fa-arrow-right next-button-arrow" aria-hidden="true" />
       </button>
     </div>
